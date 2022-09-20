b94891e3f7092802124e27948f44c09783b6744c
Walter, Mona <mona.walter@tecalliance.net>
2022-09-20 13:37:25 +0200

    Create a new txt File

---
+++ File_Handling_Tutorial/File_Handling_Tutorial/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace File_Handling_Tutorial
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            FileStream fs = new FileStream("C:\\csharpfile.txt", FileMode.Create);
+            fs.Close();
+            Console.Write("File has been created and the Path is C:\\csharpfile.txt");
+            Console.ReadKey();
+        }
+    }
+}
